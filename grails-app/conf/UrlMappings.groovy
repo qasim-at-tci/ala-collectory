3962ab21ed322b0438462b8e000d9857aa58283b
Helen Manders Jones <realhelenmjones@gmail.com>
2022-05-04 14:50:26 +0100

    CollectoryMappings.groovy

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,6 +7,11 @@ class UrlMappings {
             }
         }
 
+        "/ws/sensitiveAccess/$userId"(controller:'sensitiveAccess',action:'lookup')
+
+        "/ws/dataProvider/sensitiveSpecies/$uid" (controller:'dataProvider', action:[GET:'sensitiveSpeciesForDataProvider'])
+        "/ws/dataProvider/$uid/speciesRecords/$lsid" (controller:'dataProvider',action:'speciesRecordsForDataProvider')
+
         //"/"(view:"/index")
         "500"(view:'/error')
 	}
