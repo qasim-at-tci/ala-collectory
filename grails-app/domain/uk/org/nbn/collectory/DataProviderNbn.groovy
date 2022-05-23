6d41b655b15c1a3532989a94fbdc0cce459601bd
Helen Manders Jones <realhelenmjones@gmail.com>
2022-05-23 11:22:50 +0100

    just improving class header

--- grails-app/domain/uk/org/nbn/collectory/DataProviderNbn.groovy
+++ grails-app/domain/uk/org/nbn/collectory/DataProviderNbn.groovy
@@ -3,7 +3,7 @@ package uk.org.nbn.collectory
 import au.org.ala.collectory.ContactFor
 import au.org.ala.collectory.DataProvider
 
-class DataProviderNbn extends DataProvider {
+class DataProviderNbn extends au.org.ala.collectory.DataProvider {
     static mapping = {
         discriminator "au.org.ala.collectory.DataProvider"
     }
