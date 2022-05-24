8d83c61e37a0842fbd7795e414089d62a78ddc39
Helen Manders Jones <realhelenmjones@gmail.com>
2022-05-24 09:04:07 +0100

    extract DataProvider.groovy - review fix

--- grails-app/domain/uk/org/nbn/collectory/DataProviderNbn.groovy
+++ grails-app/domain/uk/org/nbn/collectory/DataProviderNbn.groovy
@@ -4,25 +4,12 @@ import au.org.ala.collectory.ContactFor
 import au.org.ala.collectory.DataProvider
 
 class DataProviderNbn extends au.org.ala.collectory.DataProvider {
+
     static mapping = {
         discriminator "au.org.ala.collectory.DataProvider"
     }
 
     static hasMany = [approvals: ApprovedAccess]
 
-    /**
-     * Gets a list of contacts along with their role and admin status for this group
-     *
-     */
-    @Override
-    List<ContactFor> getContacts() {
-        // handle this being called before it has been saved (and therefore doesn't have an id - and can't have contacts)
-        if (dbId()) {
-            return ContactForNbn.findAllByEntityUid(uid)
-//            return ContactFor.findAllByEntityUid(uid)
-        } else {
-            []
-        }
-    }
 
 }
