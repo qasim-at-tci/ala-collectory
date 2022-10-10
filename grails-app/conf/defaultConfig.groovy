436a10822ed39d0651eb9188e2141ae3475656cd
Helen Manders Jones <realhelenmjones@gmail.com>
2022-10-10 22:16:51 +0100

    bumped version and removed redundant file

--- grails-app/conf/defaultConfig.groovy
+++
@@ -1,6 +0,0 @@
-uploadFilePath='/data/nbn-collectory/upload'
-
-dataResourceChartsJSON = "['country','state','species_group','assertions','type_status','year','month']"
-dataProviderChartsJSON = "['country','state','species_group','assertions','type_status','year','month']"
-collectionChartsJSON  = "['country','state','species_group','assertions','type_status','year','month']"
-institutionChartsJSON  = "['country','state','species_group','assertions','type_status','year','month']"
