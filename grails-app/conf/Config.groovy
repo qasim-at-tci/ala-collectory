86bf3a03a7412d3ef0f5017bb19e6f7f2b07bbfd
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-10-10 10:16:19 +0100

    also nbn groupid

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -173,7 +173,7 @@ if (!institution.codeLoaderURL) {
 }
 
 ///******* standard grails **********/
-grails.project.groupId = 'au.org.ala' // change this to alter the default package name and Maven publishing destination
+grails.project.groupId = 'uk.org.nbn' // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = true
 grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
